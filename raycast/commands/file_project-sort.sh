69d400a3b6ff8b6fcd37dc35da7ec7f6ca2a1beb
tianli <34931204+zengtianli@users.noreply.github.com>
2026-03-25 15:06:18 +0800

    auto: sync 2026-03-25 15:06

---
+++ raycast/commands/file_project-sort.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Sort by Project
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 📁
+# @raycast.packageName File Utils
+# @raycast.description Sort renamed files into project subdirectories
+
+/Users/tianli/miniforge3/bin/python3 /Users/tianli/Dev/scripts/scripts/file/project_sort.py

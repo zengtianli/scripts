73abbb0e33520c964cb64154759ad996fa47ebed
tianli <34931204+zengtianli@users.noreply.github.com>
2026-03-25 10:43:19 +0800

    auto: sync 2026-03-25 10:43

---
+++ raycast/commands/file_organize-downloads.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Organize Downloads
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 📂
+# @raycast.packageName File Utils
+# @raycast.description Organize Downloads folder by file type
+
+/Users/tianli/miniforge3/bin/python3 /Users/tianli/Dev/scripts/scripts/file/downloads_organizer.py --scan-archive

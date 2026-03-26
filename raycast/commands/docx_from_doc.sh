b445a231e8ba370ccd4c7ee2237889c9d154577a
tianli <34931204+zengtianli@users.noreply.github.com>
2026-03-26 10:51:19 +0800

    sync: 2026-03-26 10:51

--- raycast/commands/docx_from_doc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# @raycast.schemaVersion 1
-# @raycast.title docx-from-doc
-# @raycast.description Convert .doc files to .docx format
-# @raycast.mode fullOutput
-# @raycast.icon 📝
-# @raycast.packageName Scripts
-source "$(dirname "$0")/../lib/run_python.sh" && run_python "document/docx_from_doc.py" "$@"
--- raycast/commands/docx_zdwp_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# @raycast.schemaVersion 1
-# @raycast.title docx-zdwp-all
-# @raycast.mode fullOutput
-# @raycast.icon ✨
-# @raycast.packageName Document Processing
-# @raycast.description One-click ZDWP document standardization (6 steps)
-source "$(dirname "$0")/../lib/run_python.sh" && run_python "document/docx_zdwp_all.py" "$@"

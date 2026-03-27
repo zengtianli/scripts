28c244aa1f4e766fce9eb7d570d37e91aa7fcf2e
tianli <34931204+zengtianli@users.noreply.github.com>
2026-03-27 14:30:38 +0800

    sync: 2026-03-27 14:30

--- projects/reservoir_schedule/src/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-水库发电调度核心模块
-"""
-from .hydro_core import HydroElectricity, read_info_txt, read_paras
-
-__all__ = ['HydroElectricity', 'read_info_txt', 'read_paras']
-
-
